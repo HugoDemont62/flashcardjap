1bb0946d73451580c003b6ece81c105603a2954e
HugoDemont <hugodemont62@hotmail.fr>
2024-04-19 16:59:37 +0200

    [ADD] Add jisho API in view

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'index.html')
+    data = get_data_from_jisho('日本語')  # Remplacez '日本語' par le mot que vous voulez rechercher
+    return render(request, 'index.html', {'data': data})
+
+
+def get_data_from_jisho(keyword):
+    response = requests.get(f'https://jisho.org/api/v1/search/words?keyword={keyword}')
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+
